83e79cac249bb8260d9bc0b39c408385ff52ebf2
AndreaBeltramin <andreabeltra99@gmail.com>
2025-07-04 17:18:45 +0200

    aggiunt pareggio e reset punti

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ function App() {
 	const [winner, setWinner] = useState("");
 	const [userChoice, setUserChoice] = useState("");
 	const [computerChoice, setComputerChoice] = useState("");
+	const [countUserWin, setCountUserWin] = useState(0);
+	const [countUserLose, setCountUserLose] = useState(0);
+	const [countDraw, setCountDraw] = useState(0);
 
 	// funzione per controllare il vincitore ricevendo la tabella come parametro
 	function checkWinner(board) {
@@ -29,9 +32,11 @@ function App() {
 				return board[a]; // ritorna il vincitore ( X o O)
 			}
 		}
+
 		return null; // se non c'è nessuna corrispondenza, nessun vincitore
 	}
 
+	// funzione per gestire la scelta dell'utente
 	function handleUserChoice(e) {
 		const userChoice = e.target.innerText;
 		setUserChoice(userChoice);
@@ -51,10 +56,17 @@ function App() {
 
 		if (checkWinner(newBoard)) {
 			setWinner(userChoice); // setto il vincitore
+			setCountUserWin((prev) => prev + 1);
 			// console.log("Ha vinto " + currentPlayer);
 		} else if (newBoard.every((cell) => cell !== null)) {
-			// se tutte le celle sono piene
-			setWinner("Pareggio"); // setto il pareggio
+			const possibleWinner = checkWinner(newBoard);
+			if (possibleWinner) {
+				setWinner(possibleWinner); // se c'è un vincitore, setto il vincitore
+			} else {
+				// se tutte le celle sono piene
+				setWinner("Pareggio"); // setto il pareggio
+				setCountDraw((prev) => prev + 1);
+			}
 		} else {
 			setCurrentPlayer(computerChoice); // se non c'è un vincitore allora camnio simbolo del giocatore per passare il turno al computer
 		}
@@ -64,22 +76,31 @@ function App() {
 	function makeComputerMove() {
 		const emptyCells = board
 			.map((value, index) => (value === null ? index : null))
-			.filter((index) => index !== null); // verifco le daselle vuote
+			.filter((index) => index !== null); // verifico le caselle vuote
 		if (emptyCells.length === 0) return; // se non ci sono celle vuote mi fermo, è finita la partita
 		const randomIndex =
 			emptyCells[Math.floor(Math.random() * emptyCells.length)]; // scelgo casualmente una cella per il computer
 		const newBoard = [...board];
-		newBoard[randomIndex] = computerChoice; // asssegno al computer la O
-		setBoard(newBoard); // aggiorno la tabella con la nuova tabella
+		newBoard[randomIndex] = computerChoice; // asssegno al computer la mossa
+		setBoard(newBoard); // aggiorno la tabella con la nuova tabella modificata
 
 		// controllo le varie condizioni
 		if (checkWinner(newBoard)) {
 			setWinner(computerChoice); // se vince il computer
+			setCountUserLose((prev) => prev + 1);
 		} else if (newBoard.every((cell) => cell !== null)) {
 			setWinner("Pareggio"); // se è pareggio
+			setCountDraw((prev) => prev + 1);
 		} else {
 			setCurrentPlayer(userChoice); // se non vince il computer, cambio giocatore
 		}
+	}
+
+	// funzione per resettare i punteggi
+	function handleResetPoints() {
+		setCountUserWin(0);
+		setCountUserLose(0);
+		setCountDraw(0);
 	}
 
 	// funzione per resettare il gioco
@@ -93,6 +114,16 @@ function App() {
 
 	useEffect(() => {
 		if (currentPlayer === computerChoice && !winner) {
+			const emptyCells = board.filter((cell) => cell === null);
+			if (emptyCells.length === 0) {
+				const possibleWinner = checkWinner(board);
+				if (possibleWinner) {
+					setWinner(possibleWinner);
+				} else {
+					setWinner("Pareggio");
+				}
+				return;
+			}
 			const timeOut = setTimeout(() => {
 				makeComputerMove();
 			}, 800); // rendiamo automatica la mossa del computer dopo 800 ms
@@ -101,45 +132,82 @@ function App() {
 	}, [currentPlayer, winner]);
 
 	return (
-		<div className="container m-5">
-			<h1>Tris Kata</h1>
-
-			<p>Allinea tre simboli uguali in orizzontale, verticale o diagonale!</p>
-			<p>
-				Scegli se giocare con{" "}
-				<button className="btn bg-primary p-2" onClick={handleUserChoice}>
-					X
-				</button>{" "}
-				o{" "}
-				<button className=" btn bg-info p-2" onClick={handleUserChoice}>
-					O
-				</button>
-			</p>
-			<div className="board">
-				<Board board={board} onClick={handleClick} />
+		<div className="container mx-auto mt-5">
+			<div className="text-center">
+				<h1>Tris Kata</h1>
+				<p>Allinea tre simboli uguali in orizzontale, verticale o diagonale!</p>
+				<p>
+					Scegli se giocare con{" "}
+					<button
+						className="btn btn-start bg-primary py-2"
+						onClick={handleUserChoice}
+					>
+						X
+					</button>{" "}
+					o{" "}
+					<button
+						className=" btn btn-start bg-info p-2"
+						onClick={handleUserChoice}
+					>
+						O
+					</button>
+				</p>
 			</div>
 
-			{!winner && currentPlayer !== computerChoice && (
-				<p className="p-3 bg-info">Tocca a te!</p>
-			)}
+			<div className="d-flex row gx-4">
+				<div className="d-flex col-8 flex-column align-items-center">
+					<Board board={board} onClick={handleClick} />
+					<div
+						style={{ minheight: "90px" }}
+						className="d-flex flex-column justify-content-center "
+					>
+						<div>
+							{!winner && currentPlayer !== computerChoice && (
+								<p className="p-3 bg-info">Tocca a te!</p>
+							)}
+						</div>
+						<div>
+							{winner && winner !== "Pareggio" && winner === userChoice && (
+								<p className="p-3 bg-success text-center">
+									Complimenti, hai vinto!
+								</p>
+							)}
 
-			{winner && winner !== "Pareggio" && winner === userChoice && (
-				<p className="p-3 bg-success">Complimenti, hai vinto!</p>
-			)}
+							{winner && winner !== "Pareggio" && winner === computerChoice && (
+								<p className="p-3 bg-danger text-center">
+									Mi dispiace, hai perso! Riprova
+								</p>
+							)}
 
-			{winner && winner !== "Pareggio" && winner === computerChoice && (
-				<p className="p-3 bg-danger">Mi dispiace, hai perso! Riprova</p>
-			)}
+							{winner && winner === "Pareggio" && (
+								<p className="p-3 bg-warning text-center">Pareggio!</p>
+							)}
+						</div>
 
-			{winner && winner === "Pareggio" && (
-				<p className="p-3 bg-warning">Pareggio!!</p>
-			)}
+						<p className="mb-5 text-center">
+							{winner && (
+								<button className="btn btn-primary" onClick={handleResetGame}>
+									Rinizia il gioco
+								</button>
+							)}
+						</p>
+					</div>
+				</div>
 
-			{winner && (
-				<button className="btn btn-primary" onClick={handleResetGame}>
-					Rinizia il gioco
-				</button>
-			)}
+				<div className="col-4 flex-column points ">
+					<p className="p-3 bg-success text-center">Vittorie: {countUserWin}</p>
+					<p className="p-3 bg-danger text-center">
+						Sconfitte: {countUserLose}
+					</p>
+					<p className="p-3 bg-warning text-center">Pareggi: {countDraw}</p>
+					<button
+						className="p-3 btn btn-primary text-center"
+						onClick={handleResetPoints}
+					>
+						Resetta i punteggi
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
